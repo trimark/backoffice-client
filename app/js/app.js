032de9e1ef326251564c09b70f368ae24985352e
rizalpascual <rizal_pascual@hotmail.com>
2016-10-08 02:30:12 +0800

    Initial setup of angular.material

---
+++ app/js/app.js
@@ -0,0 +1,16 @@
+angular.module('trimark-backoffice', [ 'ngMaterial']).config(['$mdIconProvider', '$mdThemingProvider',
+    function($mdIconProvider, $mdThemingProvider)
+    {
+        $mdIconProvider.defaultIconSet("./assets/svg/avatars.svg", 128)
+        .icon("menu", "./assets/svg/menu.svg", 24)
+        .icon("share", "./assets/svg/share.svg", 24)
+        .icon("google_plus", "./assets/svg/google_plus.svg", 24)
+        .icon("hangouts", "./assets/svg/hangouts.svg", 24)
+        .icon("twitter", "./assets/svg/twitter.svg", 24)
+        .icon("phone", "./assets/svg/phone.svg", 24);
+
+        $mdThemingProvider.theme('default').primaryPalette('brown')
+        .accentPalette('red');
+        console.log("Patatas");
+    }
+]);
