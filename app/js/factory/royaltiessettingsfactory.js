14fdb0003740bf760b8a780b7b383a196d23d6e3
Makac <matswidegren@gmail.com>
2016-11-11 13:13:29 +0100

    selectedTotalsData

--- app/js/factory/royaltiessettingsfactory.js
+++ app/js/factory/royaltiessettingsfactory.js
@@ -123,10 +123,10 @@ var royaltiesSettingsFactory = function () {
 				]
 			}
 		],
-		columns : ["Distributor", "Operator", "Brand", "Game", "Player Count", "Bet Count", "Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share", "Operator Share"],
-		selectedColumns : ["Distributor", "Operator", "Brand", "Game", "Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share", "Operator Share"],
-		totalsData : ["Distributor", "Operator", "Brand", "Player Count", "Bet Count", "Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share", "Operator Share"],
-		selectedTotalsData : ["Distributor", "Operator", "Brand", "Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share", "Operator Share"],
+		columns : ["Distributor", "Operator", "Brand", "Game", "Player Count", "Bet Count", "Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share"],
+		selectedColumns : ["Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share"],
+		totalsData : ["Player Count", "Bet Count", "Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share"],
+		selectedTotalsData : ["Player Count", "Bet Count", "Bets", "Wins", "Net Gaming", "Royalty", "Distributor Share", "Supplier Share"],
 		showTotals: true,
 		groupBy: "None"
 	}
